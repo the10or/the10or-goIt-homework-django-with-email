0bf0d6185c00b05bde32ca539d142ed297d9d217
the10or <the10or@gmail.com>
2024-02-05 17:08:05 +0200

    before setting .env

--- hw_quotes/users/urls.py
+++ hw_quotes/users/urls.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import (PasswordResetConfirmView, PasswordResetDoneView,
+                                       PasswordResetCompleteView)
 from django.urls import path
 
 from . import views
@@ -9,4 +11,21 @@ urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile_view, name="profile"),
+    path("reset-password/", views.ResetPasswordView.as_view(), name="password_reset"),
+    path("reset-password/done/",
+         PasswordResetDoneView.as_view(
+             template_name="users/password_reset_done.html"
+         ),
+         name="password_reset_done"),
+
+    path("reset-password/confirm/<uidb64>/<token>/",
+         PasswordResetConfirmView.as_view(
+             template_name="users/password_reset_confirm.html",
+             success_url="/users/reset-password/complete/"),
+         name="password_reset_confirm"),
+
+    path("reset-password/complete/",
+         PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+         name="password_reset_complete"),
+
 ]
--- hw_quotes/users/views.py
+++ hw_quotes/users/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from .forms import SignUpForm, LoginForm, ProfileForm
 
@@ -64,3 +67,12 @@ def profile_view(request):
         "users/profile.html",
         context={"profile_form": profile_form}
     )
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = "users/password_reset.html"
+    email_template_name = "users/password_reset_email.html"
+    html_email_template_name = "users/password_reset_email.html"
+    success_url = reverse_lazy("users:password_reset_done")
+    success_message = "Check your email to reset your password"
+    subject_template_name = "users/password_reset_subject.txt"
