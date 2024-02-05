89f32042592793f0a1d95ebb56ba937f6c15d3a3
the10or <the10or@gmail.com>
2024-02-05 17:49:17 +0200

    moved data to .env

--- hw_quotes/hw_quotes/settings.py
+++ hw_quotes/hw_quotes/settings.py
@@ -12,15 +12,19 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j&ls#0j@0qq@4p@jj#u8$7+gi%!l%y)fwiigg^9&7+@q6gc@ur"
+SECRET_KEY = env("SECRET_KEY") #"django-insecure-j&ls#0j@0qq@4p@jj#u8$7+gi%!l%y)fwiigg^9&7+@q6gc@ur"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,13 +35,13 @@ LOGIN_URL = "/users/login/"
 # Application definition
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.meta.ua'
-EMAIL_PORT = 465
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_STARTTLS = False
 EMAIL_USE_SSL = True
 EMAIL_USE_TLS = False
-EMAIL_HOST_USER = 'the10or@meta.ua'
-EMAIL_HOST_PASSWORD = 'nottea'
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
@@ -89,11 +93,11 @@ WSGI_APPLICATION = "hw_quotes.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "hw_quotes",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": env("DB_NAME"),#"hw_quotes",
+        "USER": env("DB_USER"),#"postgres",
+        "PASSWORD": env("DB_PASS"),#"postgres",
+        "HOST": env("DB_HOST"),#"localhost",
+        "PORT": env("DB_PORT"),#"5432",
     }
 }
 
